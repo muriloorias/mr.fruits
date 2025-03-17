7621094cbe00708af4d3b8d4451726a473c987c0
muriloorias <156871325+muriloorias@users.noreply.github.com>
2025-03-17 15:14:36 +0000

    melhorar initial

---
+++ js/navigation.js
@@ -0,0 +1,9 @@
+function goToBenifts(){
+    window.location.href = "benefits.html";
+}
+
+function goTorecips(){
+    Window.location.href = "recips.html";
+}
+
+document.getElementById("buttom1").addEventListener("click", goToBenifts)
