9c84f08b8b9195e490b432f9d3557beac0f9e616
muriloorias <mu.a.o@outlook.com>
2025-03-17 14:56:36 -0300

    adicionando botoes

--- js/navigation.js
+++
@@ -1,9 +0,0 @@
-function goToBenifts(){
-    window.location.href = "benefits.html";
-}
-
-function goTorecips(){
-    Window.location.href = "recips.html";
-}
-
-document.getElementById("buttom1").addEventListener("click", goToBenifts)
